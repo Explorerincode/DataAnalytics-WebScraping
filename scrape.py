dae9b82d6a199c79cb53e61c98d47c1edc99cd76
ikleerigrow <ikleerigrow@outlook.com>
2022-11-25 20:44:28 +0100

    web scraper

--- scrape.py
+++ scrape.py
@@ -8,13 +8,8 @@ for pages in range(1,5):
     page =requests.get(url+str(pages))
     soup=BeautifulSoup(page.content,'html.parser')
     lists= soup.find_all('section', class_="listing-search-item")
-    
     with open('Delft_rental_price.csv','a',encoding='utf8',newline='') as files:
-        
         thewriter=writer(files)
-        header=['title','location','price','area','room','furniture']
-        thewriter.writerow(header)
-
         for list in lists:
             title = list.find('a', class_="listing-search-item__link--title").text.replace('\n','')
             location = list.find('div', class_="listing-search-item__sub-title").text.replace('\n','')
@@ -22,8 +17,10 @@ for pages in range(1,5):
             area = list.find('li', class_="illustrated-features__item--surface-area").text.replace('\n','')
             room = list.find('li', class_="illustrated-features__item--number-of-rooms").text.replace('\n','')
             furniture = getattr(list.find('li', class_="illustrated-features__item--interior"),'text',None)
-
+            
             info =[title,location,price,area,room,furniture]
             thewriter.writerow(info)
+
+
         
 
